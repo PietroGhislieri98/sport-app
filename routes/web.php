4762dc73c2af18877163f356ff7b3c8de94ecb78
Pietro Ghislieri <pietroghislieri@gmail.com>
2025-10-22 00:08:35 +0200

    chore: apply .gitignore retroattivamente

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-
-Route::get('/', function () {
-    throw new HttpException(500, 'Not Authorized');
-});
